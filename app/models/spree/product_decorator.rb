971b16555b775ef14aff0fe2c283d016aaf0a756
Punkbooster <ugliar.arsen@gmail.com>
2019-10-02 12:41:19 +0200

    Use prepend instead of class_eval in the models/controllers

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,9 @@
 module Spree
-  Product.class_eval do
-    alias_method :amount=, :price=
+  module ProductDecorator
+    def self.prepended(base)
+      base.alias_method :amount=, :price=
+    end
   end
 end
+
+::Spree::Product.prepend ::Spree::ProductDecorator
--- lib/spree/core/controller_helpers/order_decorator.rb
+++ lib/spree/core/controller_helpers/order_decorator.rb
@@ -1,11 +1,19 @@
-Spree::Core::ControllerHelpers::Order.class_eval do
-  def current_currency
-    # ensure session currency is supported
-    #
-    if session.key?(:currency) && supported_currencies.map(&:iso_code).include?(session[:currency])
-      session[:currency]
-    else
-      Spree::Config[:currency]
+module Spree
+  module Core
+    module ControllerHelpers
+      module Order
+        def current_currency
+          # ensure session currency is supported
+          #
+          if session.key?(:currency) && supported_currencies.map(&:iso_code).include?(session[:currency])
+            session[:currency]
+          else
+            Spree::Config[:currency]
+          end
+        end
+      end
     end
   end
 end
+
+::Spree::Core::ControllerHelpers::Order.prepend ::Spree::Core::ControllerHelpers::OrderDecorator
